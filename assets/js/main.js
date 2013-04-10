9b957aa8ed0e565b8c148ff5362b4619a32e03d4
Colby Rabideau <crabideau5691@gmail.com>
2013-04-09 23:42:26 -0400

    pjax that shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-;(function ($) {
+$(function ($) {
 
-  console.log('hello, world!');
+  var $container = $('#main');
+  var $toggle = $('.navbar-toggle');
 
-}(window.jQuery));
+  $('body').on('click', 'a[href!="#"]', function (e) {
+    e.preventDefault();
+    var $tar = $(e.currentTarget);
+    if (document.documentElement.clientWidth < 756 &&
+        $tar.is('.navbar .nav a')) {
+      $toggle.click();
+    }
+    if ($tar.attr('href') !== '#') {
+      $.get('/partials' + $tar.attr('href'), function (data) {
+        $container.html(data);
+      });
+    }
+  });
+
+});
