5fae41b05a8069dee8cb839d80c10852bf102b0a
Colby Rabideau <idontknow5691@gmail.com>
2013-04-16 03:23:13 -0400

    search flow

---
+++ assets/js/fixedlist.js
@@ -0,0 +1,37 @@
+;(function ($) {
+
+  var data = {};
+
+  $.fn.fixedlist = function () {
+
+    var $input = this.find('input');
+    var $list = this.find('.list');
+    var $placeholder = this.find('.placeholder');
+    var type = $input.data('type');
+
+    $input.focus();
+
+    if (data.hasOwnProperty(type)) {
+      $input.val(data[type]);
+    }
+
+    $input.keyup(function () {
+      var val = $input.val().toLowerCase().trim();
+      $list.find('.list-group-item').each(function () {
+        var $el = $(this);
+        if (this.innerText.toLowerCase().trim().indexOf(val) === -1) {
+          $el.hide();
+        } else {
+          $el.show();
+        }
+      });
+    });
+
+    $list.on('click', '.list-group-item a', function (e) {
+      data[type] = this.innerText;
+      console.log(data);
+    });
+
+  };
+
+}(window.jQuery));
